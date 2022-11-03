b1454f602aee7e357a0777fa434e86fa29dd6df1
Enow Divine <sirdivine16@gmail.com>
2022-11-03 07:43:51 +0100

    dashboard cards

--- src/pages/account/dashboard/Dashboard.jsx
+++ src/pages/account/dashboard/Dashboard.jsx
@@ -1,10 +1,39 @@
 import "./Dashboard.css";
 import Layout from "../../../components/layout/Layout";
+import { FaUsers } from "react-icons/fa";
+import DashboardCards from "../../../components/account/dashboardCards/DashboardCards";
 
 const Dashboard = () => {
   return (
     <Layout>
-      <div className="dashboard-wrapper">Dashboard</div>
+      <div className="dashboard-wrapper">
+        <div className="dashboard-cards">
+          <DashboardCards
+            icon={<FaUsers size={35} />}
+            title="NUMBER OF USER"
+            value={10265}
+            bgColor="lightblue"
+          />
+          <DashboardCards
+            icon={<FaUsers size={35} />}
+            title="NUMBER OF TRANSACTIONS"
+            value={10265}
+            bgColor="lightgreen"
+          />
+          <DashboardCards
+            icon={<FaUsers size={35} />}
+            title="PENDING TRANSACTIONS"
+            value={10265}
+            bgColor="orange"
+          />
+          <DashboardCards
+            icon={<FaUsers size={35} />}
+            title="TOTAL INCOME"
+            value={10265}
+            bgColor="blue"
+          />
+        </div>
+      </div>
     </Layout>
   );
 };
